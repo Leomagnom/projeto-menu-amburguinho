37e2097984341489b3c857dc6ccaa8df8c8cf7c1
Leomagnom <leomagnom@gmail.com>
2023-12-23 11:11:19 -0300

    aula 7

---
+++ aula7/index.js
@@ -0,0 +1,9 @@
+// String = text |fora de aspas Number= Número 
+//Operadores aritimeticos ->  + - */
+const primeiroNumero = 5; // Number
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado= resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado +5;
+console.log(typeof(primeiroNumero + segundoNumero));

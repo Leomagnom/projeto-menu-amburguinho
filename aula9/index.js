2f1e52c5d266b2adcbfdcfa47201ba34d40779f1
Leomagnom <leomagnom@gmail.com>
2023-12-23 14:12:10 -0300

    aula9 atualização

--- aula9/index.js
+++ aula9/index.js
@@ -1,3 +1,14 @@
 // com  a variável let, não pode delcarar e redeclarar 
 // com var sim.
 // de preferêcia a let.
+// String, number, undefined, null , boolean, symbol -> (são dados primitivos em javaScript)
+const nome = 'Luiz';// String
+const nome1 = "Luiz";// String
+const nome2 = `Luiz`;// String
+const num1 = 10; // number
+const num2 = 10.52; // number flutuânte
+let nomeAluno; // undefined -> nçao aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum na memória 
+const aprovado = false; //boolean = true, false (logíco)
+console.log(typeof aprovado ,aprovado)
+

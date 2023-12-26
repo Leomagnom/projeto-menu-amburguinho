a3ba1fdc2c5d8e771a0db365dba06a33290cba7d
Leomagnom <leomagnom@gmail.com>
2023-12-26 11:26:29 -0300

    aula 10

---
+++ aula10/index.js
@@ -0,0 +1,45 @@
+/**
+ *  Aritméticos
+ * + Adição / Concatenção (- / *)
+ * ** Potenciação
+ * % Resto da divisão
+ */ 
+
+ /** A prossedecias dos operadores aritméticos
+  * primeiro os -> ()
+  * segundo a ponteciação -> ** 
+  * terceiro -> * /  modulo da divisão ->%
+  * quarto -> + -
+  */
+
+/*const num1 = 5;
+const num2 = 2;
+const num3 =10;
+console.log(num1 * num2 / num3);*/
+
+/** let contador = 1;
+++contador; //2 operador de incremento
+++contador; //3 operador de incremento
+++contador; //4 operador de incremento
+++contador; //5 operador de incremento
+console.log(contador); 
+
+
+ */
+ /* 
+let contador = 10;
+console.log(--contador);
+console.log(--contador)
+*/
+
+/*
+Incremento = ++
+Decremento = --
+*/ 
+//NaN => Not a number, 
+//parseInt => (inteiro), 
+//parseFloat => (decimais), 
+//Number => Numero
+const num1 = 10;
+const num2 = Number('5.2');
+console.log(num1 + num2);

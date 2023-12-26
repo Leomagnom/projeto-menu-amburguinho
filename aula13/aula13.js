5778b42e5273cb1418b0e466a5e136c6a3eeac5d
Leomagnom <leomagnom@gmail.com>
2023-12-26 15:07:59 -0300

    aula13

---
+++ aula13/aula13.js
@@ -0,0 +1,10 @@
+// 0123456789
+let umaString ="O rato roeu a roupa do rei de roma."; 
+
+//console.log(umaString.replace(/r/g, '#')); 
+//console.log(umaString.length);// aqui mostra o tamanho da frase
+//console.log(umaString.slice(-5, -1)); //tem o mesmo resultodo abaixo
+//console.log(umaString.substring(umaString.length - 5, umaString.length - 1)); // tem o mesmo resultado do acima
+
+  console.log(umaString.toUpperCase());
+  console.log(umaString.toLowerCase());

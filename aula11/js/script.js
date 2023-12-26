b2d0531e79baec54d062370c27fb77b43a7f8a3f
Leomagnom <leomagnom@gmail.com>
2023-12-26 11:26:45 -0300

    aula 11

---
+++ aula11/js/script.js
@@ -0,0 +1,13 @@
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado da sua conta foi:  ${num1 + num2}`);
+
+
+
+/*const num1 = Number (10);
+const num2 = Number(500);
+console.log(num1 + num2);*/

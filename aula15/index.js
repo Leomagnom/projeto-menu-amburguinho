c3a1e00801e139003117c48339f943ef7438ebe1
Leomagnom <leomagnom@gmail.com>
2023-12-27 11:05:04 -0300

    aula15 uso de Math

---
+++ aula15/index.js
@@ -0,0 +1,15 @@
+ //let num1 = 9.54578;
+ //let num2 = Math.floor(num1);//-> arendonda pra baixo,inguinorão do as casas decímas
+ //let num2 = Math.ceil(num1);//-> aredonda pra cima
+ //let num2 = Math.round(num1);//-> aredonda para o mais proximo
+ // console.log(num2);
+ //console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));//-> mostra o maior
+ //console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));//-> mostra o menor
+ //const aleatorio = Math.round(Math.random() * (10 - 5) + 5);//-> gera números aleatórios, e arendodado
+ //console.log(Math.PI)//->valor de 'PI'
+ //console.log(Math.pow(2, 10));//->resoltado igual o abaixo (potênciação)
+ //console.log(2 ** 10);//->resultado igual acima (potênciação)
+ //let num1 = 50;
+// console.log(num1 ** (1/2));//-> número que eu quero pegar raiz quadrada elevado a 1/2 (1 dividido por 2)
+ //console.log(num1 ** 0.5);//-> resultado igual o acima
+ 

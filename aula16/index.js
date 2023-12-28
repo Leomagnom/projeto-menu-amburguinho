51160bf03ab3e3f5a3e69ccf22c3601f63717b84
Leomagnom <leomagnom@gmail.com>
2023-12-28 10:35:01 -0300

    aula16  trabalhando com array

---
+++ aula16/index.js
@@ -0,0 +1,43 @@
+//               0       1        2          3      4
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo -> são separados por(,)são indequiçados 
+
+console.log(typeof alunos); // mostra que é um objeto
+console.log(alunos instanceof Array); // confirma se é um array
+//alunos.push('Luiza');
+//alunos.push('Eduardo');
+
+//console.log(alunos.slice(0, 3));// slice = fatiar positivo -> esquerda/direita
+//console.log(alunos.slice(0, -1)); // Slice = fatiar negativo -> direita/esquerda 
+
+//console.log(alunos[50]); // Algo que não existe gerá -> undefined 
+
+//delete alunos[1]; // Delete
+//console.log(alunos[1]);
+
+//const removido = alunos.shift(); // Remolvido começo
+//console.log(removido);
+//console.log(alunos);
+
+//const removido = alunos.pop(); // Removi do fim
+
+//alunos.unshift('Fábio'); // Adiciona no começo
+//alunos.unshift('Luiza');
+
+
+
+//alunos.push('Luiza'); // Adiciona no fim
+//alunos.push('Fábio');
+
+//alunos[alunos.length] = 'Luiza';//Adiciona no fim,e mostra quantas casas estão ocupadas
+//alunos[alunos.length] = 'Fábio';
+//alunos[alunos.length] = 'Luana';
+
+
+//alunos[0] ='Eduardo'; Altera
+//alunos[3] = 'Luiza'; Adicionando
+
+
+//console.log(alunos);
+ //console.log(alunos[0]);
+ //console.log(alunos[1]);
+ //console.log(alunos[2]);

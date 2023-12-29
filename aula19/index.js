27f043fb0deaf4a6add602ac6873849bbf660ad6
Leomagnom <leomagnom@gmail.com>
2023-12-29 13:54:51 -0300

    aula19: uso de ' spread = espalhar'

---
+++ aula19/index.js
@@ -0,0 +1,20 @@
+/**
+ * Primitivos (imutáveis) - string, numbean, boolean, undefined, null (bigint, symbol )-> Valores copiados
+
+   Referência (mutável) - array, object, function ->Passados por referência
+*/
+
+let a = [1, 2, 3];
+let b = [...a]; // ->spread = espalhar
+let c = b;
+
+console.log(a, b,);
+
+a.push(4); 
+console.log(a, b, );
+
+b.pop();
+console.log(a, b);
+
+a.push('Luiz');
+console.log(a,c);

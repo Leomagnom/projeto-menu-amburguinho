d98f4eae2000a4c5b53bc33fc24d59a5d618099b
Leomagnom <leomagnom@gmail.com>
2023-12-30 08:14:44 -0300

    aula 22: Operadores Lógicos

---
+++ aula22/index.js
@@ -0,0 +1,8 @@
+/** 
+ * Operadores Lógicos
+ * && -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+ * || -> OR -> OU -> As expressões precisam ser diferentes
+ * ! -> NOT -> NÂO -> Converte a expressão true e false, e false e true 
+ */
+
+

9c99b107c513336392af8a904e2becaa1a7d97f9
Leomagnom <leomagnom@gmail.com>
2023-12-30 07:53:48 -0300

    aula21 Operadores  de comparação

---
+++ aula21/index.js
@@ -0,0 +1,11 @@
+/**
+ * Operadorres de comparação
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <=menor que ou igual a
+ * ==igualdade (valor)****************-> não é usado
+ * === igualdade estrita (valor e tipo)
+ * != diferente (valor)***************->não é usado
+ * !== diferente estrito (valor e tipo)
+ */

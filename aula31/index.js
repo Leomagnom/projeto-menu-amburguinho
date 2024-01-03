00cfbb229359e58727081e5be97307c0200d23ca
Leomagnom <leomagnom@gmail.com>
2024-01-03 08:38:56 -0300

    aula 31: diferença  entre let e var

---
+++ aula31/index.js
@@ -0,0 +1,22 @@
+// const verdadeira = true;
+
+// //Let tem escopo de bloco {...bloco}, -> qualquer para de {} é um escopo pra Let. 
+// //Var só tem escopo de função // -> pra Var só teria escopo de função.
+
+// let nome = 'Luiz'; // criando
+// var nome2 = 'Luiz';// criando
+
+
+// if (verdadeira){
+//     let nome = 'Otávio'; // criando
+//     var nome2 = 'Rogério'; //redeclarando
+//    // console.log(nome, nome2);
+   
+//     if(verdadeira){ //-> escopo aninhado em bloco
+//         var nome2= 'Ronaldo'; // -> redeclarada      
+//         let nome = 'Outra coisa';
+//      //  console.log(nome, nome2); // -> esta sendo usado dentro do aninhamento
+//     }
+// }
+// console.log(nome, nome2); // -> sendo usado no escopo global.
+

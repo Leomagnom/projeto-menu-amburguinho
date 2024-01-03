6894d394f1b99a979cc1905fdfbf22e00ee19eb8
Leomagnom <leomagnom@gmail.com>
2024-01-03 16:16:20 -0300

    aula36: uso de for in

---
+++ aula36/index.js
@@ -0,0 +1,26 @@
+// for in -> lê os índeces ou chavas do objeto
+//                0       1      2
+const pessoa = {
+    nome:'Luiz',
+    sobrenome:'Otávio',
+    idade: 30
+};
+  
+for (let chave in pessoa ) {
+    console.log(chave,pessoa[chave]);
+}
+
+// for (let indice in frutas){
+//     console.log(frutas[indice] );
+// }
+
+
+
+
+
+
+
+
+// for(let i = 0; i < frutas.length; i++) {
+// console.log(frutas[i]);
+// }

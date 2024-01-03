d04ea4417805dc6b6bd894e3ad6b8d6731e52277
Leomagnom <leomagnom@gmail.com>
2024-01-03 18:59:55 -0300

    Aula 38: uso de for of , para mudar  o style do projeto com js

--- aula38/assets/js/main.js
+++ aula38/assets/js/main.js
@@ -4,7 +4,7 @@ const ps = paragrafos.querySelectorAll('p');
 const estilosBody = getComputedStyle(document.body);
 const backgroundColorBodoy = estilosBody.backgroundColor;
 console.log(backgroundColorBodoy);
-
+// aqui abaixo, usei For of para mudar a cor do projeto
 for(let p of ps){//-> Usei essa função para mudar a cor style do projeto 
     p.style.backgroundColor = backgroundColorBodoy;
     p.style.color = '#ffffff';

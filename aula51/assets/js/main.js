7b02fb6cff5937207ce1bf83504fe9492c8cb5c0
Leomagnom <leomagnom@gmail.com>
2024-02-07 20:45:29 -0300

    Retorno da função

---
+++ aula51/assets/js/main.js
@@ -0,0 +1,12 @@
+// return
+// Retorna um valor
+// Termina a função
+function soma (a, b){
+    return a + b;
+}
+console.log(soma(5, 2))
+
+function soma2(a, b) {
+    console.log(a + b);
+}
+soma2(5, 2);

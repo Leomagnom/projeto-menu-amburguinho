e28eede841c1c2f3257a968a8c81b32877e50ba4
Leomagnom <leomagnom@gmail.com>
2024-02-15 19:03:06 -0300

    alua72 Get e Set

--- aula72/assets/js/main.js
+++ aula72/assets/js/main.js
@@ -1,21 +1,40 @@
 // defineProperty-> getter e Setters
-function Produto(nome, preco, estoque) {//->dessa forma o código fica mais compaquito
+function Produto(nome, preco, estoque) { 
   this.nome =nome;
   this.preco = preco;
  let estoquePrivado = estoque;
  Object.defineProperty(this,'estoque', {
   enumerable:true,
   configurable: true,
-  get: function(){
+  get: function(){ // Quando esta usando uma function Construtora, é assim de usa Get e Set.
     return estoquePrivado;
   },
    set: function(valor) {
+    if (typeof valor !== 'number') {
+      throw new TypeError('Mensagem');
+    }
      estoquePrivado = valor;
    }
  });
 }
 
 const p1 = new Produto('Camiseta',20, 3);
-console.log(p1);
-p1.estoque = 'O valor que eu quero';
+// console.log(p1);
+p1.estoque = 500;
 console.log(p1.estoque);
+//------------------------------------------------------------///
+function CriaProduto(nome) {//->Get e Set na função factury
+  return {
+    get nome() {//-> obtem o valor. 
+      return nome;      
+    },
+    set nome(valor) {//-> seta o valor.
+      valor = valor.replace('coisa.','');
+      nome = valor;
+    }
+  };
+}
+
+const p2 = CriaProduto('Camiseta');
+p2.nome = 'qualquer coisa.';
+console.log(p2.nome);

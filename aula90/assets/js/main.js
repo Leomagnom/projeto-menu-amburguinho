b4eb8eb2d272420f82b61bc869c7b57b010587d9
Leomagnom <leomagnom@gmail.com>
2024-03-04 08:35:12 -0300

    aula90
    Fetch API (GET)

---
+++ aula90/assets/js/main.js
@@ -0,0 +1,93 @@
+// XMLHttpRequest //->Essa função pode fazer requisição de qualque tipo de dado: "HTML","JSON","XML","TEXTO PURO", etc..
+
+//Esse é o modo novo com Promise
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
+
+//         xhr.addEventListener('load', () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
+//             } else {
+//                 reject(xhr.statusText);
+//             }
+//         });
+//     });
+
+// };
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+  try {
+    const href = el.getAttribute('href');
+    const response = await fetch(href);
+
+    if (response.status !== 200) throw new Error('ERRO 404!');
+
+    const html = await response.text();
+    carregaResultado(html);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
+}
+
+//Esse abaixo modo antigo, de fazer o código
+// const request = obj => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(obj.method, obj.url, true);
+//     xhr.send();
+
+//     xhr.addEventListener('load', () => {
+//         if (xhr.status >= 200 && xhr.status < 300) {
+//             obj.success(xhr.responseText);
+//         } else {
+//             obj.error(xhr.statusText);
+//         }
+//     });
+// };
+
+// document.addEventListener('click', e => {
+//     const el = e.target;
+//     const tag = el.tagName.toLowerCase();
+
+//     if (tag === 'a') {
+//         e.preventDefault();
+//         carregaPagina(el);
+//     }
+// });
+
+// function carregaPagina(el) {
+//     const href = el.getAttribute('href');
+//     console.log(href);
+
+//     request({
+//         method: 'GET',
+//         url: href,
+//         success(response) {
+//             carregaResultado(response);
+//         },
+//         erro(errorText) {
+//             console.log(errorText);
+//         }
+//     });
+// }
+// function carregaResultado(response) {
+//    const resultado = document.querySelector('.resultado');
+//    resultado.innerHTML = response;
+// }

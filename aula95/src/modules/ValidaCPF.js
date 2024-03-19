536d6af492ea64257ea9b4155c360eef9cc1db74
Leomagnom <leomagnom@gmail.com>
2024-03-19 18:07:57 -0300

    aula95
    Gerador de CPF

---
+++ aula95/src/modules/ValidaCPF.js
@@ -0,0 +1,46 @@
+//705.484.450-52 070.987.720-03
+export default class ValidaCPF {
+    constructor(cpfEnviado)  {
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable:true,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        });
+    
+    }
+        eSequemcia() {
+            return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+        }    
+          
+        geraNovoCpf() {//Aqui vou fazer uma flag
+          const cpfSemDigitos = this.cpfLimpo.slice(0, -2);  
+          const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+          const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+          this.novoCPF = cpfSemDigitos + digito1 + digito2;
+        }
+
+         static geraDigito(cpfSemDigitos) {//Metodo static dentro da minha class, porque não esta usando nem uma (this).
+            let total = 0;
+            let reverso = cpfSemDigitos.length + 1 ;
+
+            for(let stringNumerica of cpfSemDigitos) {       
+                total += reverso * Number(stringNumerica);
+                reverso--;
+            }
+           const digito = 11 - (total % 11);
+           return digito <= 9 ? String(digito) : '0';
+        }
+
+        valida() {
+          if(!this.cpfLimpo) return false;
+          if(typeof this.cpfLimpo !== 'string') return false
+          if(this.cpfLimpo.length !== 11) return false;
+          if(this.eSequemcia()) return false;
+          if(!this.geraNovoCpf()); 
+         
+          return this.novoCPF === this.cpfLimpo;
+        }
+}
+
+

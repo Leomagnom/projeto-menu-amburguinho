385e41c3fff59c944cfc0cf6930d2bb349742792
Leomagnom <leomagnom@gmail.com>
2024-03-25 20:54:36 -0300

    aula97
    122. Core-js e regenerator-runtime

---
+++ aula97/src/main.js
@@ -0,0 +1,9 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+//os dois acima garante que vai rodar em navegadores mais antigos.
+import executa from './modulos/promises';
+
+
+import './assets/css/style.css';
+
+executa();

372526f35b66f4a2a8e7c8d85515051bc0d1c7cf
Leomagnom <leomagnom@gmail.com>
2024-04-17 20:55:22 -0300

    aula6 de node
    aula sobre Get Post Update Delete

--- node/aula6/server.js
+++ node/aula6/server.js
@@ -9,7 +9,15 @@ const app = express();
 // http://meusite.com/contato <- GET -> Entregue a página /contato
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send(`<form action="/" method="POST">
+  Nome: <input type="text" name="nome">
+  <button>Enviar</button>
+  </form>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Recebi o formulário');
 });
 
 app.get('/contato', (req, res) => {
@@ -17,6 +25,6 @@ app.get('/contato', (req, res) => {
 });
 
 app.listen(3000, () =>{
-    console.log('Acessar http>//localhost:3000');
-    console.log('Servidor executando  porta 3000');
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
 } );

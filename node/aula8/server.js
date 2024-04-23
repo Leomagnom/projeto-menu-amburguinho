ea4bf83d0ab2afbd06dfc024d00a8e59948f187f
Leomagnom <leomagnom@gmail.com>
2024-04-22 21:00:50 -0300

    aula8
    enviando e recebendo requisição

--- node/aula8/server.js
+++ node/aula8/server.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended:true}));//->faz o tratamento do body.
+
 // /profiles/12345? -> parametros de url
 // campanha=googleads & -> curry strings, utilisando express
 // nome_campanha=seila
 
 app.get('/', (req, res) => {
   res.send(`<form action="/" method="POST">
-  Nome do cliente: <input type="text" name="nome">
+  Nome do cliente: <input type="text" name="nome"> //=> aqui manda pra
   <button>Olá mundo da progamação, estou chegando</button>
   </form>
   `);
 });
 
 app.get('/testes/:idUsuarios?/:parametros?', (req, res) => {
+  //-> /profiles/3
+  //-> /profile/?chave1=valor1&chave2=valor2&chave3=valor3
+  //
 console.log(req.params);
+console.log(req.query);
 res.send(req.params);
 });
 
 app.post('/', (req, res) => {
-  res.send('Recebi o formulário');
+  console.log(req.body);
+  res.send(`O que você me enviou foi: ${req.body.nome}`);//cá
 });
 
 

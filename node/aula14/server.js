e3e84b2823df355d4a5ce2e5ebdb85d2f23899a9
Leomagnom <leomagnom@gmail.com>
2024-06-03 19:31:05 -0300

    aula 14

---
+++ node/aula14/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const connectionString = 'mongodb+srv://<username>:<password>@cluster0.lou7htw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;'
+mongoose.connect(connectionString);
+
+
+
+
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Nossos próprios middlewares
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.listen(3000, () => { 
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+} ); 
